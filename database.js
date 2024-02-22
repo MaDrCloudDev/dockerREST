50903ee8a65344aa0900d1719596fa227ad06551
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2024-02-22 16:56:51 -0700

    init

---
+++ database.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+const { user, password } = require('pg/lib/defaults');
+
+const pool = new Pool({
+	host: 'db',
+	port: 5432,
+	user: 'user123',
+	password: 'password123',
+	database: 'db123',
+});
