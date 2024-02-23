b45ad79451b1710b6ebc54711ada2e03679c6943
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2024-02-22 18:00:13 -0700

    docker, node, express, crud

--- server.js
+++ server.js
@@ -5,27 +5,27 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 
-// routes
+//routes
 app.get('/', async (req, res) => {
 	try {
 		const data = await pool.query('SELECT * FROM schools');
 		res.status(200).send(data.rows);
 	} catch (err) {
-		console.error(err.message);
+		console.log(err);
 		res.sendStatus(500);
 	}
 });
 
 app.post('/', async (req, res) => {
 	const { name, location } = req.body;
-
 	try {
 		await pool.query(
-			'INSERT INTO schools (name, address) VALUES ($1, $2), [name, location]'
+			'INSERT INTO schools (name, address) VALUES ($1, $2)',
+			[name, location]
 		);
-		res.status(200).send({ message: 'School added' });
+		res.status(200).send({ message: 'Successfully added child' });
 	} catch (err) {
-		console.error(err.message);
+		console.log(err);
 		res.sendStatus(500);
 	}
 });
@@ -33,15 +33,15 @@ app.post('/', async (req, res) => {
 app.get('/setup', async (req, res) => {
 	try {
 		await pool.query(
-			'CREATE TABLE IF NOT EXISTS schools (id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100)'
+			'CREATE TABLE schools( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))'
 		);
-		res.status(200).send({ message: 'School table created' });
+		res.status(200).send({ message: 'Successfully created table' });
 	} catch (err) {
-		console.error(err.message);
+		console.log(err);
 		res.sendStatus(500);
 	}
 });
 
 app.listen(port, () =>
-	console.log(`Server is running on port ${port}`)
+	console.log(`Server has started on port: ${port}`)
 );
