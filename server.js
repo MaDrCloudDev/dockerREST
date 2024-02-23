a090722d659f2ad36824591fa5af76010025b1b2
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2024-02-22 17:24:18 -0700

    sql query

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(express.json());
 
 // routes
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
 	res.sendStatus(200);
 });
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
 	const { name, location } = req.body;
 
 	// Add logging to inspect req.body
@@ -19,6 +19,17 @@ app.post('/', (req, res) => {
 	res.status(200).send({
 		message: `Your keys were ${name} and ${location}!`,
 	});
+});
+
+app.get('/setup', async (req, res) => {
+	try {
+		await pool.query(
+			'CREATE TABLE IF NOT EXISTS schools (id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100)'
+		);
+	} catch (err) {
+		console.error(err.message);
+		res.sendStatus(500);
+	}
 });
 
 app.listen(port, () =>
